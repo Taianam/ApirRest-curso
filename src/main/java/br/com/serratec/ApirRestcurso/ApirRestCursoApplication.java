3ea0a2ae90b2973778d2c6977ff466e6518a0196
Taianam <taiana.monteirof@gmail.com>
2021-06-08 08:48:21 -0300

    Initial commit

---
+++ src/main/java/br/com/serratec/ApirRestcurso/ApirRestCursoApplication.java
@@ -0,0 +1,13 @@
+package br.com.serratec.ApirRestcurso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirRestCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirRestCursoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serratec/ApirRestcurso/ApirRestCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serratec.ApirRestcurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirRestCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
